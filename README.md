02bde6cc2964fe7e274ac707dc8fdbb3d07f13a9
vermad843 <vishnuverma7524@gmail.com>
2021-04-10 01:17:48 +0530

    Login

--- README.md
+++ README.md
@@ -13,3 +13,8 @@
 [x] React Hooks Functional(Hooks, State)
 
 [x] Entire React Tech Stack
+
+[x] Client ID 6982637b5a9c40278ff6f530c1fc2cf7
+
+[x] https://developer.spotify.com/ 
+
--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Login from './Login';
 
 function App() {
   return (
     <div className="app">
       <h1>Hello World!!!</h1>
+        {/* Spotify logo */}
+        {/* login with Spotify button  */}
+      <Login/>
     </div>
   );
 }
